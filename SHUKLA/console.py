2fd8066521eb76db0b2c06c571592a2d4155a313
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-09-19 14:25:52 +0530

    Update console.py

--- SHUKLA/console.py
+++ SHUKLA/console.py
@@ -28,16 +28,16 @@ if os.path.exists("Internal"):
    load_dotenv("Internal")
 
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
-OWNER_ID = int(getenv("OWNER_ID", "6762113050"))
-OWNER_USERNAME = getenv("OWNER_USERNAME", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6919199044").split()))
-ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg")
+API_ID = int(getenv("API_ID", "10873743"))
+API_HASH = getenv("API_HASH", "da8ce86d450cbf063480fd96799b4be6")
+BOT_TOKEN = getenv("BOT_TOKEN", "7209856039:AAFH5FrVO5vDAFVXqeQ9RgcMFfmSyH4z5po")
+STRING_SESSION = getenv("STRING_SESSION", "BQCl648AQTBBQISdKMBzpUxXggc6Zw9HutRxh0MdN2qsE3SYls5LF-rkI8zBmFlurbW5rqT-O_nfr53NNqe_Luq1TJIJPHOoDDYGcliAK4swKAfZ_qQbX787dRV3lY7qmqSwurdXkyjfVkgA6MsHnobkNpbotajL4uL0T2dfOE9j9sokldeRl34kTM_iU2R1cuR1xqtLUhmOQ8TiPR71C1nxvnl3m2nJxE3GX_1XUZqIZ4teBVPEQmbewvpW0yfY8VbZ1Ij2XOIAm4af2NPa3tNd1oBtAzVm4P09I3arrTOEdoT5P6Fs73ym5Yu46oeM-tQZVMzUE-LTXvOzeA8PQ3HNqmNbMgAAAAGSUQ9QAA")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Lucky:Lucky@atlascluster.f7lck9c.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002021288289"))
+OWNER_ID = int(getenv("OWNER_ID", "5247304559"))
+OWNER_USERNAME = getenv("OWNER_USERNAME", "itz_Lucky_Raja")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6615076069").split()))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/fbb992b2477e3ecd09fc0.jpg")
 
 
 # OPTIONAL VARIABLES
@@ -49,13 +49,13 @@ COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
 # OTHERS VARIABLES
 
 # PM GUARD VARS
-PM_GUARD = bool(getenv("PM_GUARD", True))
+PM_GUARD = bool(getenv("PM_GUARD", False))
 PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 ʜᴇʏ, ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ & ꜱᴜᴘᴇʀꜰᴀꜱᴛ ʜɪɢʜ Qᴜᴀʟɪᴛʏ ᴜꜱᴇʀʙᴏᴛ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴡɪᴛʜ ᴀɴ ᴜᴘɢʀᴀᴅᴇᴅ ᴠᴇʀꜱɪᴏɴ ꜱᴇᴄᴜʀɪᴛʏ ꜱʏꜱᴛᴇᴍ.\n\n🌿 ɪ ᴄᴀɴ'ᴛ ʟᴇᴛ ʏᴏᴜ ᴍᴇꜱꜱᴀɢᴇ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴅᴍ ᴡɪᴛʜᴏᴜᴛ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴘᴇʀᴍɪꜱꜱɪᴏɴ.\n\n❤️ ᴍʏ ᴏᴡɴᴇʀ ɪꜱ ᴏꜰꜰʟɪɴᴇ ɴᴏᴡ, ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴜɴᴛɪʟ ᴍʏ ᴏᴡɴᴇʀ ᴀʟʟᴏᴡꜱ ʏᴏᴜ.\n\n🍂 ᴘʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ꜱᴘᴀᴍ ʜᴇʀᴇ, ʙᴇᴄᴀᴜꜱᴇ ꜱᴘᴀᴍᴍɪɴɢ ᴡɪʟʟ ꜰᴏʀᴄᴇ ᴍᴇ ᴛᴏ ʙʟᴏᴄᴋ ʏᴏᴜ ꜰʀᴏᴍ ᴍʏ ᴏᴡɴᴇʀ ɪᴅ 👍🏻**")
 PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
 
 
 # USERBOT DEFAULT IMAGE
-USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg")
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/fbb992b2477e3ecd09fc0.jpg")
 
 
 
